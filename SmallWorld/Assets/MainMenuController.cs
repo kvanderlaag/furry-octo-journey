16d33bd02bc843db47e121b04f6d42256c87cfa0
Dylan Gedig <dylan@rednexus.games>
2017-04-21 21:54:25 -0700

    Set up grody main menu and placeholder scenes.

---
+++ SmallWorld/Assets/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+	
+	public void PlayLocal() {
+		// TODO-DG: Go to local scene
+		SceneManager.LoadScene("PlanetLoad");
+	}
+
+	public void PlayMulti() {
+		// TODO-DG: Show additional options for multiplayer setup.
+	}
+}
